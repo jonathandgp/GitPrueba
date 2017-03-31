479a5dee847f28bf1ea4ea7389aef1c9ef048222
Jonathan Gomez <jonathandgp@me.com>
2017-03-31 15:02:47 +0100

    mod 1

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //modificacion Jonathan Gomez
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
